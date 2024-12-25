ffe1439533fd874ff6f9d820f8a551eb2a2da5e1
hussienkamalelden <hussien.kamal92@gmail.com>
2024-12-25 17:36:51 +0200

    Update WorkTogether component: Adjust hover effects by moving hover styles to the parent div for improved user interaction

--- src/components/home/WorkTogether.jsx
+++ src/components/home/WorkTogether.jsx
@@ -1,7 +1,7 @@
 export default function WorkTogether() {
   return (
-    <section className="bg-[#15161f] py-6 hover:bg-gradient-to-r hover:from-[#ED0C93] hover:to-[#C80CED] ">
-      <div className="mx-auto py-10  border-t-[1px] border-b-[1px] border-[#444655]">
+    <section className="bg-[#15161f] py-6  ">
+      <div className="mx-auto py-10  border-t-[1px] border-b-[1px] border-[#444655] hover:bg-gradient-to-r hover:from-[#ED0C93] hover:to-[#C80CED]">
         <h2 className="text-2xl  font-[font-b] text-[#F6F6FE] text-center ">
           Let's work together 🙌
         </h2>
